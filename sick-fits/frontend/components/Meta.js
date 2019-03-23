a548755f51ce58caaefe6cfc4f0a165c8d28bc26
HaQadosch Berraka <haqadosch@pm.me>
2019-03-23 15:32:42 +0000

    Module 2 Lesson 7, Fixes REACT-R.

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export const Meta = _ => (
   <Head>
     <meta name='viewport' content='with-device-width, initial-scale=1' />
-    <meta charset='utf-8' />
+    <meta charSet='utf-8' />
     <link rel='shortcut icon' href='/static/favicon.jpg' />
     <link rel='stylesheet' href='/static/nprogress.css' />
     <title>Sick Fits!</title>
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,24 @@
 import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
 
 export const Nav = _ => (
-  <div>
+  <NavStyles>
+    <Link href='/items'>
+      <a>Items</a>
+    </Link>
     <Link href='/sell'>
-      <a>Sell page</a>
+      <a>Sell</a>
     </Link>
-    <Link href='/'>
-      <a>Home...</a>
+    <Link href='/signup'>
+      <a>Sign up</a>
     </Link>
-  </div>
+    <Link href='/orders'>
+      <a>Orders</a>
+    </Link>
+    <Link href='/me'>
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 )
 
 export default Nav
