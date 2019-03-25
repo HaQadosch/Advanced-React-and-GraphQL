44d6a83f99731c8d048b232a0184397436240ef4
HaQadosch Berraka <haqadosch@pm.me>
2019-03-25 20:25:07 +0000

    Module 4 Lesson 15

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import App, { Container } from 'next/app'
 import * as Sentry from '@sentry/browser'
 import { Page } from '../components/Page'
+import { ApolloProvider } from 'react-apollo'
+import { withData } from '../lib/withData'
 
 Sentry.init({
   dsn: 'https://6066970219f84edca9f1c5d21e8a75c3@sentry.io/1417349',
@@ -15,7 +17,13 @@ Sentry.configureScope((scope) => {
   })
 })
 
-export default class MyApp extends App {
+export default withData(class MyApp extends App {
+  static async getInitialProps ({ Component, ctx }) {
+    const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+    pageProps.query = ctx.query
+    return pageProps
+  }
+
   constructor (props) {
     super(props)
     this.state = { error: null }
@@ -32,7 +40,7 @@ export default class MyApp extends App {
   }
 
   render () {
-    const { Component } = this.props
+    const { Component, apollo, pageProps } = this.props
 
     if (this.state.error) {
       // render fallback UI
@@ -42,11 +50,14 @@ export default class MyApp extends App {
     } else {
       return (
         <Container>
-          <Page>
-            <Component />
-          </Page>
+          <ApolloProvider client={apollo}> {/** this.props.apollo is available thanks to WithData() */}
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </ApolloProvider>
         </Container>
       )
     }
   }
 }
+)
