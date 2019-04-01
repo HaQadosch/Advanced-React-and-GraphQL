cdfdf99389ce7abfcb64090b1535515ad84664c5
HaQadosch Berraka <haqadosch@pm.me>
2019-04-01 15:42:58 +0100

    Module 4 Lesson 19

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,13 @@ const Mutation = {
   async createItem (parent, args, { db: { mutation, query, ...dbRest }, ...ctx }, info) {
     const item = await mutation.createItem({ data: { ...args } }, info)
     return item
+  },
+  updateItem (parent, { id, ...args }, { db: { mutation: { updateItem, ...mutation }, ...db }, ...ctx }, info) {
+    const update = args
+    return updateItem({
+      data: update,
+      where: { id }
+    }, info)
   }
 }
 
